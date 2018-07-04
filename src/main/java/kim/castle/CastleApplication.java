c00945ccf29dc0fef91e72737859dbcea9eaa370
xiangxik <156801554@qq.com>
2018-07-04 09:22:52 +0800

    banner

--- src/main/java/kim/castle/CastleApplication.java
+++ src/main/java/kim/castle/CastleApplication.java
@@ -1,12 +1,12 @@
 package kim.castle;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class CastleApplication {
 
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(CastleApplication.class).run(args);
+		SpringApplication.run(CastleApplication.class, args);
 	}
 }
